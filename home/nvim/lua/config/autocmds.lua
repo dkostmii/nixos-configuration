dec88bdd9586c213d8fc9f61a95985a82c08bdd8
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2024-09-03 11:22:03 +0300

    home/nvim: improve whitespace highlighting

--- home/nvim/lua/config/autocmds.lua
+++ home/nvim/lua/config/autocmds.lua
@@ -1,54 +1,56 @@
-local trailingwhitespacehighlight_group = vim.api.nvim_create_augroup('TrailingWhitespaceHighlight', {})
+local whitespacehighlight_group = vim.api.nvim_create_augroup('WhitespaceHighlight', {})
 
 local exclude_filetypes = {
   'dashboard',
   'neo-tree',
 }
 
-vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufNewFile', 'BufReadPost', 'UIEnter', 'WinEnter' }, {
   pattern = '*',
   callback = function()
     if vim.bo.buftype ~= '' then
       return
     end
 
+    local bufnr = vim.api.nvim_get_current_buf()
+    local win_id = vim.fn.bufwinid(bufnr)
+
     for _, filetype_to_exclude in ipairs(exclude_filetypes) do
       if string.find(vim.bo.filetype, filetype_to_exclude) ~= nil then
-        vim.notify(vim.bo.filetype)
+        if vim.b.trailing_whitespace_match_id ~= nil then
+          vim.fn.matchdelete(vim.b.trailing_whitespace_match_id, win_id)
+          vim.b.trailing_whitespace_match_id = nil
+        end
         return
       end
     end
 
-    local bufnr = vim.api.nvim_get_current_buf()
-    vim.b.trailing_whitespace_match_id = vim.fn.matchadd('TrailingWhitespace', '\\s\\+$', 0, -1, { bufnr = bufnr })
+    vim.b.trailing_whitespace_match_id = vim.fn.matchadd('TrailingWhitespace', '\\s\\+$', 0, -1, { window = win_id })
   end,
-  group = trailingwhitespacehighlight_group,
+  group = whitespacehighlight_group,
 })
 
-vim.api.nvim_create_autocmd({ 'FileType' }, {
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufNewFile', 'BufReadPost', 'UIEnter', 'WinEnter' }, {
   pattern = '*',
   callback = function()
-    if vim.b.trailing_whitespace_match_id == nil then
+    if vim.bo.buftype ~= '' then
       return
     end
 
-    if vim.bo.buftype == '' then
-      return
-    end
+    local bufnr = vim.api.nvim_get_current_buf()
+    local win_id = vim.fn.bufwinid(bufnr)
 
-    local is_excluded_filetype = false
     for _, filetype_to_exclude in ipairs(exclude_filetypes) do
       if string.find(vim.bo.filetype, filetype_to_exclude) ~= nil then
-        is_excluded_filetype = true
+        if vim.b.inter_word_whitespace_match_id ~= nil then
+          vim.fn.matchdelete(vim.b.inter_word_whitespace_match_id, win_id)
+          vim.b.inter_word_whitespace_match_id = nil
+        end
+        return
       end
     end
 
-    if not is_excluded_filetype then
-      return
-    end
-
-    vim.fn.matchdelete(vim.b.trailing_whitespace_match_id)
-    vim.b.trailing_whitespace_match_id = nil
+    vim.b.inter_word_whitespace_match_id = vim.fn.matchadd('InterWordWhitespace', '\\(\\S\\)\\@<=\\s\\{2,\\}\\(\\S\\)\\@=', 0, -1, { window = win_id })
   end,
-  group = trailingwhitespacehighlight_group,
+  group = whitespacehighlight_group,
 })
--- home/nvim/lua/config/highlights.lua
+++ home/nvim/lua/config/highlights.lua
@@ -1,1 +1,2 @@
 vim.cmd 'highlight TrailingWhitespace ctermbg=red guibg=red ctermfg=white guifg=white'
+vim.cmd 'highlight InterWordWhitespace ctermbg=yellow guibg=yellow ctermfg=black guifg=black'
