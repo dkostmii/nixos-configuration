07849d31faa2f8c46833e2aa6d7ef1dee79c9300
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2024-09-08 13:16:51 +0300

    home/fish: update prompt

--- home/fish/nix-shell-prompt-indicator/init.fish
+++ home/fish/nix-shell-prompt-indicator/init.fish
@@ -5,8 +5,29 @@ function fish_prompt
         end
     )
 
+    set -l git_info (
+      if git rev-parse --is-inside-work-tree &>/dev/null
+        set branch (git symbolic-ref --short HEAD 2>/dev/null)
+
+        if test -n "$branch"
+          set_color magenta
+          echo -n "("
+          echo -n $branch
+          echo -n ") "
+          set_color normal
+        end
+      end
+    )
+
+    set -l time_info (
+      set_color green
+      echo -n "$(date +'%H:%M:%S') "
+      set_color normal
+    )
+
     set_color $fish_color_cwd
-    echo -n (prompt_pwd)
+    echo -n -s "$(prompt_pwd) "
     set_color normal
-    echo -n -s " $nix_shell_info ~>"
+
+    echo -n -s "$nix_shell_info" "$git_info" "$time_info" "~> "
 end
